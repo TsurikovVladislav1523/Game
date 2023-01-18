0f69b68f592ee295c1efb873c2c111fb74bb4e95
vinny <viknikgon@yandex.ru>
2023-01-19 01:26:54 +0300

    Босс(погоня за персонажем + картинки)

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 import sys
 from config import *
 
-current_level = 'level1'
+current_level = 'level3'
 
 size = width, height = W_WIDTH, W_HEIGHT
 screen = pygame.display.set_mode(size)
@@ -20,6 +20,7 @@ player = pygame.sprite.Group()
 furniture = pygame.sprite.Group()
 monsters = pygame.sprite.Group()
 blood = pygame.sprite.Group()
+boss = pygame.sprite.Group()
 
 
 def load_image(name, color_key=None):
@@ -432,6 +433,164 @@ class AnimatedSpriteZombi(pygame.sprite.Sprite):
         blood.draw(screen)
 
 
+class ZombiBoss(pygame.sprite.Sprite):
+    def __init__(self, sheet_b, sheet_z, sheet_p, group, tp):
+        super().__init__(group)
+        self.frames = []
+        self.sheet_b = sheet_b
+        self.sheet_z = sheet_z
+        self.sheet_p = sheet_p
+        self.cut_sheet(sheet_b, tp[0], tp[1])
+        self.x0 = tp[4]
+        self.y0 = tp[5]
+        self.x1 = tp[6]
+        self.y1 = tp[7]
+        self.hp = LEVEL_ZOMBIE_HP[current_level]
+        self.view = 0
+        self.right = True
+        self.down = False
+        self.up = False
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.image = self.image.convert_alpha()
+        self.rect = pygame.Rect(0, 0, 40, 46)
+        self.mask = pygame.mask.from_surface(self.image)
+        self.speedx = 1
+        self.speedy = 1
+        # для отстлеживания ударов о мебель
+        self.count = 0
+        self.rect = self.rect.move(tp[2], tp[3])
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.frames.clear()
+        rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                           sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (rect.w * i, rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, rect.size)))
+
+    def update(self):
+        if self.hp <= 0:
+            self.kill()
+        # урон от соприкосновения
+        rect_x, rect_y = p.coords()
+        if abs(self.rect.x - rect_x) <= 10 and abs(self.rect.y - rect_y) <= 10:
+            self.attack(self.right, self.rect, p.rect.centerx - 20, p.rect.centery - 11)
+            p.hp -= 1
+            p.update(0, heal=True)
+        self.view += 1
+        if self.view % 10 == 0:
+            if self.up and not self.down:
+                self.cut_sheet(self.sheet_z, 3, 1)
+                self.cur_frame = 0
+                self.image = self.frames[self.cur_frame]
+                self.image = self.image.convert_alpha()
+            if self.down and not self.up:
+                self.cut_sheet(self.sheet_p, 3, 1)
+                self.cur_frame = 0
+                self.image = self.frames[self.cur_frame]
+                self.image = self.image.convert_alpha()
+            if not self.down and not self.up:
+                self.cut_sheet(self.sheet_b, 3, 1)
+                self.cur_frame = 0
+                self.image = self.frames[self.cur_frame]
+                self.image = self.image.convert_alpha()
+            self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+            self.image = self.frames[self.cur_frame]
+            self.image = self.image.convert_alpha()
+            if not self.right:
+                self.image = pygame.transform.flip(self.image, True, False)
+        if not (self.x0 <= rect_x <= self.x1 and self.y0 <= rect_y <= self.y1):
+            self.rect.x += self.speedx
+            self.rect.y += self.speedy
+            if pygame.sprite.spritecollideany(self, furniture):
+                self.count += 1
+                self.speedx = -self.speedx
+                if self.right:
+                    self.image = pygame.transform.flip(self.image, True, False)
+                    self.right = False
+                elif not self.right:
+                    self.image = pygame.transform.flip(self.image, True, False)
+                    self.right = True
+                if self.count > 1:
+                    self.speedy = -self.speedy
+                    self.speedx = -self.speedx
+                    if self.right:
+                        self.image = pygame.transform.flip(self.image, True, False)
+                        self.right = False
+                    elif not self.right:
+                        self.image = pygame.transform.flip(self.image, True, False)
+                        self.right = True
+                    self.count = 0
+            if self.rect.right >= self.x1:
+                if self.right:
+                    self.image = pygame.transform.flip(self.image, True, False)
+                    self.right = False
+                self.speedx -= 2
+            if self.rect.left <= self.x0:
+                if not self.right:
+                    self.image = pygame.transform.flip(self.image, True, False)
+                    self.right = True
+                self.speedx += 2
+            if self.rect.top >= self.y0:
+                self.speedy -= 2
+            if self.rect.bottom <= self.y1:
+                self.speedy += 2
+        else:
+            if self.rect.x != rect_x and self.rect.y != rect_y:
+                if self.rect.x > rect_x and self.rect.y > rect_y:
+                    if self.right:
+                        self.image = pygame.transform.flip(self.image, True, False)
+                        self.right = False
+                    self.rect.x -= 1
+                    self.rect.y -= 1
+                elif self.rect.x < rect_x and self.rect.y < rect_y:
+                    if not self.right:
+                        self.image = pygame.transform.flip(self.image, True, False)
+                        self.right = True
+                    self.rect.x += 1
+                    self.rect.y += 1
+                elif self.rect.x < rect_x and self.rect.y > rect_y:
+                    if not self.right:
+                        self.image = pygame.transform.flip(self.image, True, False)
+                        self.right = True
+                    self.rect.x += 1
+                    self.rect.y -= 1
+                elif self.rect.x > rect_x and self.rect.y < rect_y:
+                    if self.right:
+                        self.image = pygame.transform.flip(self.image, True, False)
+                        self.right = False
+                    self.rect.x -= 1
+                    self.rect.y += 1
+            elif self.rect.y != rect_y:
+                if self.rect.y < rect_y:
+                    self.rect.y += 1
+                    self.down, self.up = True, False
+                elif self.rect.y > rect_y:
+                    self.rect.y -= 1
+                    self.down, self.up = False, True
+            elif self.rect.x != rect_x:
+                if self.rect.x < rect_x:
+                    if not self.right:
+                        self.image = pygame.transform.flip(self.image, True, False)
+                        self.right = True
+                    self.rect.x += 1
+                elif self.rect.x > rect_x:
+                    if self.right:
+                        self.image = pygame.transform.flip(self.image, True, False)
+                        self.right = False
+                    self.rect.x -= 1
+                self.down, self.up = False, False
+
+    def attack(self, dir, z_rect, p_x, p_y):
+        p.hp -= LEVEL_DAMAGE[current_level]
+        Blood(blood, dir, z_rect, p_x, p_y)
+        blood.draw(screen)
+
+
+
 def terminate():
     pygame.quit()
     sys.exit()
@@ -545,6 +704,7 @@ def load_level():
     player.empty()
     furniture.empty()
     monsters.empty()
+    boss.empty()
     blood.empty()
     global board, gate, gun, cur, p
     board = Board()
@@ -554,7 +714,11 @@ def load_level():
     board.render()
     p = AnimatedSprite(load_image("walk.png"), load_image("down.png"), load_image("up1.png"), 5, 1, 100, 100)
     for elem in ZOMBIE_COORDS[current_level]:
-        z = AnimatedSpriteZombi(load_image("zombi.png"), monsters, elem)
+        if current_level == 'level3':
+            z = ZombiBoss(load_image("boss_bok.png"), load_image('boss_zad.png'), load_image('boss_pered.png'), boss, elem)
+        else:
+            z = AnimatedSpriteZombi(load_image("zombi.png"), monsters, elem)
+
 
 
 running = True
@@ -592,11 +756,13 @@ while running:
     furniture.draw(screen)
     gates.draw(screen)
     monsters.update()
+    boss.update()
     player.draw(screen)
     smokes.update(p.rect.center)
     smokes.draw(screen)
     blood.update()
     monsters.draw(screen)
+    boss.draw(screen)
     cursor1.update(pygame.mouse.get_pos())
     cursor1.draw(screen)
     health.draw(screen)
